9e84d1a0575d4bf7a9005611aaf3bb171a5f18aa
Jose Aguilar <jaguilarny@gmail.com>
2022-06-08 18:10:57 -0400

    add style.css with some initial styling, add fetch and display search results functions in index.js

--- index.js
+++ index.js
@@ -11,20 +11,45 @@ function performSearch(event) {
     const dropdown = document.querySelector('#search-categories');
     switch (dropdown.value) {
         case 'city':
-            endpoint =`https://api.openbrewerydb.org/breweries?by_city=${searchInput.value}&per_page=all`;
+            endpoint = `https://api.openbrewerydb.org/breweries?by_city=${searchInput.value}&per_page=all`;
             break;
         case 'state':
-            endpoint =`https://api.openbrewerydb.org/breweries?by_state=${searchInput.value}&per_page=all`;
+            endpoint = `https://api.openbrewerydb.org/breweries?by_state=${searchInput.value}&per_page=all`;
             break;
         case 'zipcode':
-            endpoint =`https://api.openbrewerydb.org/breweries?by_postal=${searchInput.value}&per_page=all`;
+            endpoint = `https://api.openbrewerydb.org/breweries?by_postal=${searchInput.value}&per_page=all`;
              break;
         case 'term':
-            endpoint = `https://api.openbrewerydb.org/breweries/search?query=${searchInput.value}&per_page=all`;
+            endpoint = `https://api.openbrewerydb.org/breweries/?by_name=${searchInput.value}&per_page=all`;
     }
 
-    //TODO: fetch and display search results
+    fetch(endpoint)
+        .then(res => res.json())
+        .then(data => {
+            document.querySelector('#results').innerHTML = '';
+            data.forEach(result => createBreweryCard(result))
+        })
     document.querySelector('#search-form').reset();
 };
 
+function createBreweryCard(result) {
+    const resultsDiv = document.querySelector('#results');
+    const childDiv = document.createElement('div');
+    childDiv.setAttribute('class', 'brewcard')
+    //Name
+    const name = document.createElement('p');
+    name.setAttribute('class', 'brewname');
+    name.textContent = `${result.name}`;
+    //Address
+    const address = document.createElement('p');
+    address.textContent = `Address: ${result.street || 'N/A'} ${result.city}, ${result.state} ${result.postal_code}`
+    //Phone
+    const phone = document.createElement('p');
+    phone.textContent = `Phone: ${result.phone || 'N/A'}`
+    //Website
+    const website = document.createElement('p')
+    website.textContent = `Website: ${result.website_url || 'N/A'}`
 
+    childDiv.append(name, address, phone, website)
+    resultsDiv.appendChild(childDiv);
+};
