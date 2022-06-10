6823a2f1cf820dbf1b70c8d9123f372ec8f48559
Jose Aguilar <jaguilarny@gmail.com>
2022-06-10 16:56:36 -0400

    updated createCard function to exclude breweries that are no longer open

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function performSearch(event) {
             break;
         case 'zipcode':
             endpoint = `https://api.openbrewerydb.org/breweries?by_postal=${searchInput.value}&per_page=all`;
-             break;
+            break;
         case 'name':
             endpoint = `https://api.openbrewerydb.org/breweries/?by_name=${searchInput.value}&per_page=all`;
     }
@@ -32,35 +32,37 @@ function performSearch(event) {
 };
 
 function createCard(result) {
-    const resultsDiv = document.querySelector('#results');
-    const childDiv = document.createElement('div');
-    //Format 10 digit phone numbers to include dashes
-    const phoneFormat = function(input) {
-        if (input < 10) return null
-        return input.slice(0,3) + '-' + input.slice(3,6) + '-' + input.slice(-4);
-    };
-    childDiv.setAttribute('class', 'brewcard')
-    //Name
-    const name = document.createElement('p');
-    name.setAttribute('class', 'brewname');
-    name.textContent = `${result.name}`;
-    //Address
-    const address = document.createElement('p');
-    address.textContent = `Address: ${result.street || 'N/A'} ${result.city}, ${result.state} ${result.postal_code}`
-    //Phone
-    const phone = document.createElement('p');
-    phone.textContent = `Phone: ${phoneFormat(result.phone )|| 'N/A'}`
-    //Website
-    const website = document.createElement('p')
-    website.textContent = `Website: ${result.website_url || 'N/A'}`
-    //Google Maps
-    const map = document.createElement('a');
-    if (result.latitude && result.longitude) {
-        map.setAttribute('href', `https://www.google.com/maps/place/${result.latitude},${result.longitude}`)
-        map.setAttribute('target', '_blank');
-        map.textContent = 'See location on Google Maps'
-    };
-    
-    childDiv.append(name, address, phone, website, map)
-    resultsDiv.appendChild(childDiv);
+    if (result.brewery_type !== "closed") {
+        const resultsDiv = document.querySelector('#results');
+        const childDiv = document.createElement('div');
+        childDiv.setAttribute('class', 'brewcard')
+        //Format 10 digit phone numbers to include dashes
+        const phoneFormat = function (input) {
+            if (input < 10) return null
+            return input.slice(0, 3) + '-' + input.slice(3, 6) + '-' + input.slice(-4);
+        };
+        //Name
+        const name = document.createElement('p');
+        name.setAttribute('class', 'brewname');
+        name.textContent = `${result.name}`;
+        //Address
+        const address = document.createElement('p');
+        address.textContent = `Address: ${result.street || 'N/A'} ${result.city}, ${result.state} ${result.postal_code}`
+        //Phone
+        const phone = document.createElement('p');
+        phone.textContent = `Phone: ${phoneFormat(result.phone) || 'N/A'}`
+        //Website
+        const website = document.createElement('p')
+        website.textContent = `Website: ${result.website_url || 'N/A'}`
+        //Google Maps
+        const map = document.createElement('a');
+        if (result.latitude && result.longitude) {
+            map.setAttribute('href', `https://www.google.com/maps/place/${result.latitude},${result.longitude}`)
+            map.setAttribute('target', '_blank');
+            map.textContent = 'See location on Google Maps'
+        };
+
+        childDiv.append(name, address, phone, website, map)
+        resultsDiv.appendChild(childDiv);
+    }
 };
